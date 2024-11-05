b4d1f88d92357c6c700131c209e8244b57725dfc
kimjack <rlawor95@naver.com>
2024-11-06 00:10:26 +0900

    캐릭터 리깅 + HMD 연동 1차 완료

--- InteractiveVR-Quest2_/Assets/Scripts/RiggingManager.cs
+++ InteractiveVR-Quest2_/Assets/Scripts/RiggingManager.cs
@@ -6,18 +6,22 @@ public class RiggingManager : MonoBehaviour
 {
     public Transform leftHandIK;
     public Transform rightHandIK;
-
+    public Transform headIK;
+    
+    [Space(10)]
     public Transform leftHandController;
     public Transform rightHandController;
-
+    public Transform hmd;
+    
     public Vector3[] leftOffset;
     public Vector3[] rightOffset;
-
+    public Vector3[] headOffset;
 
     private void LateUpdate()
     {
         MappingHandTransform(leftHandIK, leftHandController, true);
         MappingHandTransform(rightHandIK, rightHandController, false);
+        MappingHeadTransform(headIK, hmd);
     }
 
     private void MappingHandTransform(Transform ik, Transform controller, bool isleft)
@@ -26,5 +30,11 @@ public class RiggingManager : MonoBehaviour
 
         ik.position = controller.TransformPoint(offset[0]);
         ik.rotation = controller.rotation * Quaternion.Euler(offset[1]);
+    }
+
+    private void MappingHeadTransform(Transform ik, Transform hmd)
+    {
+        ik.position = hmd.TransformPoint(headOffset[0]);
+        ik.rotation = hmd.rotation * Quaternion.Euler(headOffset[1]);
     }
 }
